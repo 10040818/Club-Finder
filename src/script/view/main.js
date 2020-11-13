227fb0b9bed3fa9dd686e3ea381b65659549f7c4
Club Finder <kumpultugas17@gmail.com>
2020-11-13 13:07:06 +0800

    Aplikasi Club Finder

---
+++ src/script/view/main.js
@@ -0,0 +1,40 @@
+const main = () => {
+    const searchElement = document.querySelector("#searchElement");
+    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const clubListElement = document.querySelector("#clubList");
+
+    const onButtonSearchClicked = () => {
+        const dataSource = new DataSource(renderResult, fallbackResult);
+        dataSource.searchClub(searchElement.value);
+    };
+
+    const renderResult = function (results) {
+        clubListElement.innerHTML = "";
+        results.forEach(function (club) {
+
+            const {name, fanArt, description} = club;
+
+
+            // function expression
+            // const sayHello = greet => console.log(`${greet}!`);
+            // const sayName = name => console.log(`Nama saya ${name}`);
+
+            const clubElement = document.createElement("div");
+            clubElement.setAttribute("class", "club");
+
+            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+            <div class="club-info">
+            <h2>${name}</h2>
+            <p>${description}</p>
+            </div>`;
+            clubListElement.appendChild(clubElement);
+        })
+    };
+
+    const fallbackResult = function (message) {
+        clubListElement.innerHTML = "";
+        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
+    };
+
+    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+};
